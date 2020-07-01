53e610132816a66c9c34ca3f5d4da6f7a0a3d466
Felipe Ferreira <felipe.ferreira1003@gmail.com>
2020-07-01 17:16:12 -0300

    Iniciando projeto de programa para seleção de tipo de sinal

---
+++ choose_signal.asm
@@ -0,0 +1,8 @@
+
+_main:
+
+;choose_signal.c,1 :: 		void main() {
+;choose_signal.c,3 :: 		}
+L_end_main:
+	GOTO        $+0
+; end of _main
---
+++ choose_signal.cp
@@ -0,0 +1,4 @@
+#line 1 "C:/Users/João/Documents/Programação/PIC/alternador de sinais/choose_signal.c"
+void main() {
+
+}
